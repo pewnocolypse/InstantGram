9b576af1541f4e72a3934fb679ae413e9eae39d1
pewnocolypse <alqaisinisreen@gmail.com>
2016-06-09 10:11:46 -0500

    Add files via upload

---
+++ app/controllers/greetings_controller.rb
@@ -0,0 +1,5 @@
+class GreetingsController < ApplicationController
+  def hello
+  	@message = "Welcome to InstantGram. Sign up below to post pictures and share them with your friends!"
+  end
+end
---
+++ app/controllers/sign_controller.rb
@@ -0,0 +1,12 @@
+class SignController < ApplicationController
+  def Up
+  	
+  #	attr_accessible :attribute1, attribute2
+  	
+
+  	#validates_confirmation_of :password
+  end
+
+  def Page
+  end
+end
